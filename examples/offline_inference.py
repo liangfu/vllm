ed36239c7597f78ad0f2d0e682552f8fa2dcdcd8
Liangfu Chen <liangfc@amazon.com>
2023-11-17 06:49:04 +0000

    update offline inference script for transformers-neuronx inference

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,3 +1,4 @@
+import os
 from vllm import LLM, SamplingParams
 
 # Sample prompts.
@@ -10,8 +11,18 @@ prompts = [
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '12355'
+os.environ['PATH'] = "/home/ubuntu/workspace/vllm/venv/bin:/home/ubuntu/.local/bin:/opt/aws/neuron/bin:/opt/amazon/openmpi/bin/:/opt/amazon/efa/bin/:/opt/aws/neuron/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+os.environ['NEURONX_DUMP_TO'] = os.getcwd()
+
+os.environ["NEURON_RT_DBG_EMBEDDING_UPDATE_BOUND_CHECK"] = "0"
+os.environ["NEURON_RT_DBG_INDIRECT_MEMCPY_BOUND_CHECK"] = "0"
+os.environ["NEURON_CC_FLAGS"]= " -O1 --tensorizer-options=' --no-run-pg-layout-and-tiling ' --internal-backend-options=' --enable-indirect-memcpy-bound-check=false ' "
+
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m")
+# llm = LLM(model="facebook/opt-125m")
+llm = LLM(model="openlm-research/open_llama_3b", tensor_parallel_size=2, max_num_seqs=32, max_model_len=128, block_size=128)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
