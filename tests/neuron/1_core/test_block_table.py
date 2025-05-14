ac5625eac2392feddfa7276a52bcbff42466fb9a
Liangfu Chen <liangfc@amazon.com>
2025-05-14 22:43:34 +0000

    bug fix in block-table test

--- tests/neuron/1_core/test_block_table.py
+++ tests/neuron/1_core/test_block_table.py
@@ -30,8 +30,9 @@ def nki_load_and_transform_block_tables(
 ):
     assert is_power_of_2(
         num_blocks_per_tile), f"{num_blocks_per_tile=} must be power of 2"
+    # load_active_block_tables(block_tables_hbm, num_tiles, num_blocks_per_tile):
     block_tables_sbuf = load_active_block_tables(
-        block_tables, context_lens, block_size, num_tiles, num_blocks_per_tile)
+        block_tables, num_tiles, num_blocks_per_tile)
 
     # we need to pass an Index as head_id
     head_id = nl.arange(1)[None, :] + head_id
@@ -39,15 +40,15 @@ def nki_load_and_transform_block_tables(
     block_tables_transposed = transform_block_tables_for_indirect_load(
         block_tables_sbuf,
         block_size_tiling_factor, num_head, head_id)
-    assert block_tables_transposed.shape[1] == B_P_SIZE
+    assert block_tables_transposed.shape[0] == B_P_SIZE
 
     out = nl.ndarray(
         block_tables_transposed.shape,
         dtype=nl.int32,
         buffer=nl.shared_hbm,
     )
-    for i in nl.affine_range(block_tables_transposed.shape[0]):
-        nl.store(dst=out[i], value=block_tables_transposed[i])
+    # for i in nl.affine_range(block_tables_transposed.shape[0]):
+    nl.store(dst=out, value=block_tables_transposed)
     return out
 
 
@@ -61,32 +62,43 @@ def ref_block_tables_transform(
     head_id,
     block_size_tiling_factor,
 ):
-    assert block_tables.numel() == num_tiles * num_blocks_per_tile
-    block_tables = block_tables.view(num_tiles, num_blocks_per_tile)
-    num_tiles_padded = round_up(num_tiles, B_P_SIZE)
-    block_tables = F.pad(
-        block_tables,
-        (0, 0, 0, num_tiles_padded - num_tiles),
-        "constant",
-        0,
-    )
+    # assert block_tables.numel() == num_tiles * num_blocks_per_tile
+    # block_tables = block_tables.view(num_tiles, num_blocks_per_tile)
+    # num_tiles_padded = round_up(num_tiles, B_P_SIZE)
+    # block_tables = F.pad(
+    #     block_tables,
+    #     (0, 0, 0, num_tiles_padded - num_tiles),
+    #     "constant",
+    #     0,
+    # )
+    # num_tiles, num_blocks_per_tile = block_tables.shape
+    B_P_SIZE = 128
+    if num_tiles > B_P_SIZE and num_tiles % B_P_SIZE != 0:
+        num_tiles_padded = round_up(num_tiles, B_P_SIZE)
+        block_tables = F.pad(
+            block_tables,
+            (0, 0, 0, num_tiles_padded - num_tiles),
+            "constant",
+            0,
+        )
+        num_tiles = num_tiles_padded
 
     block_tables = block_tables * num_head + head_id
-    block_tables = block_tables.view(num_tiles_padded, num_blocks_per_tile, 1)
+    block_tables = block_tables.view(num_tiles, num_blocks_per_tile, 1)
     offset = torch.arange(0, block_size_tiling_factor).view(1, 1, -1)
     block_tables = block_tables * block_size_tiling_factor + offset
-    block_tables_transposed = block_tables.view(num_tiles_padded, -1).t()
+    block_tables_transposed = block_tables.view(num_tiles, -1).t()
 
     num_blocks_per_tile = block_tables_transposed.shape[0]
     assert num_blocks_per_tile % B_P_SIZE == 0
     return block_tables_transposed.view(num_blocks_per_tile // B_P_SIZE,
-                                        B_P_SIZE, num_tiles_padded)
+                                        B_P_SIZE, num_tiles).transpose(0, 1)
 
 
 @pytest.mark.parametrize(
     "q_head_per_kv_head,head_id",
     [
-        # (1, 0),
+        (1, 0),
         (3, 1),
     ],
 )
@@ -94,12 +106,12 @@ def ref_block_tables_transform(
     "num_tiles,num_blocks_per_tile",
     [
         (13, 16),
-        # (17, 128),
-        # (35, 512),
-        # (128, 128),
-        # (130, 64),
-        # (280, 256),
-        # (315, 1),
+        (17, 128),
+        (35, 512),
+        (128, 128),
+        (130, 64),
+        (280, 256),
+        (315, 1),
     ],
 )
 @torch.inference_mode()
@@ -113,7 +125,7 @@ def test_load_and_transform_block_tables(
     import torch_xla.core.xla_model as xm
 
     device = xm.xla_device()
-    num_seqs = min(num_tiles, 4)
+    num_seqs = 1 # min(num_tiles, 4)
     block_size = 16
     context_lens = torch.tensor([12, 22, 32, 152], dtype=torch.int32, device=device)
 
@@ -137,11 +149,33 @@ def test_load_and_transform_block_tables(
         block_tables = torch.randint(
             0,
             max_num_blocks,
-            (num_seqs, num_tiles * num_blocks_per_tile),
+            (num_tiles, num_blocks_per_tile),
             dtype=torch.int32,
         )
+
+        # build active block table
+        # TODO(liangfu): move this implementation into NKI kernel
+        # num_blocks = 2048 // block_size
+        # num_blocks_per_seq = (context_lens + block_size - 1) // block_size
+        # active_block_table = torch.zeros(num_blocks * 2,
+        #                                  dtype=torch.int32,
+        #                                  device=context_lens.device)
+        # indices = torch.arange(num_blocks, device=context_lens.device)
+        # offset = torch.tensor(
+        #     0, dtype=torch.int32,
+        #     device=context_lens.device)  # Start after the initial zeros
+        # for seq_idx in range(num_seqs):
+        #     blocks_for_seq = num_blocks_per_seq[seq_idx]
+        #     active_block_table.index_put_((indices[:blocks_for_seq] + offset, ),
+        #                                   block_tables[seq_idx, :blocks_for_seq])
+        #     offset += blocks_for_seq
+        # active_block_table = active_block_table[:num_blocks]
+        # assert active_block_table.shape[
+        #     0] == 128, f"invalid active_blocks_table shape: {active_block_table.shape=}"
+
+
         nki_out = nki.jit(nki_load_and_transform_block_tables)[1, 1](
-            block_tables.to(device=device),
+            block_tables.flatten().to(device=device),
             context_lens,
             block_size,
             num_tiles,
